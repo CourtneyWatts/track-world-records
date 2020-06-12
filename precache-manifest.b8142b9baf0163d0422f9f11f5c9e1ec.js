eb98917ef3967129c00bb6a8f6cf8e5bf8cbc935
CourtneyWatts <courtney.watts@icloudm>
2020-06-12 16:30:45 +0100

    Updates

---
+++ precache-manifest.b8142b9baf0163d0422f9f11f5c9e1ec.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c804af6e415de11cb1e76c44a72c67",
+    "url": "/track-world-records/index.html"
+  },
+  {
+    "revision": "14b733c6664daf535d91",
+    "url": "/track-world-records/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b792475996085fed5aaf",
+    "url": "/track-world-records/static/css/main.e32ad209.chunk.css"
+  },
+  {
+    "revision": "14b733c6664daf535d91",
+    "url": "/track-world-records/static/js/2.6293fdc6.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/track-world-records/static/js/2.6293fdc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b792475996085fed5aaf",
+    "url": "/track-world-records/static/js/main.9ce425a7.chunk.js"
+  },
+  {
+    "revision": "e162546296353ab70804",
+    "url": "/track-world-records/static/js/runtime-main.a1ed003f.js"
+  },
+  {
+    "revision": "1df9fc561f596e5862a9269b7eb3b5a6",
+    "url": "/track-world-records/static/media/Almaz_Ayana.18373466.jpg"
+  },
+  {
+    "revision": "11659c30f3f9a04e63a44076d32e0dd0",
+    "url": "/track-world-records/static/media/Bekele.c72d4d98.jpg"
+  },
+  {
+    "revision": "c849ac8cfd0a7761d8befd1bc34dbd10",
+    "url": "/track-world-records/static/media/Czech_Republic.c849ac8c.svg"
+  },
+  {
+    "revision": "75d140eb7f3b3dd4d401c6fe77dda624",
+    "url": "/track-world-records/static/media/David_Rudisha.c873bdf3.jpg"
+  },
+  {
+    "revision": "7aa11d7cf15deacf02d2142fccd9cc3e",
+    "url": "/track-world-records/static/media/East_Germany.7aa11d7c.svg"
+  },
+  {
+    "revision": "6ad9189472189b268802498bb37bd0b9",
+    "url": "/track-world-records/static/media/Flo_Jo.814f37c9.jpg"
+  },
+  {
+    "revision": "7d7130bc9a00f443c9e7c24128877493",
+    "url": "/track-world-records/static/media/Genzebe_Dibaba.0ae5b5c6.jpg"
+  },
+  {
+    "revision": "b875859a5e9f7e94f0fec533bfc7a5fd",
+    "url": "/track-world-records/static/media/Guerrouj.28c02b4b.jpg"
+  },
+  {
+    "revision": "6dd461d21a21e3821701599bfbcdef7b",
+    "url": "/track-world-records/static/media/Jarmila_Kratochvlov.709cf2ef.jpg"
+  },
+  {
+    "revision": "38be8aa5b764902564181ab663dc765b",
+    "url": "/track-world-records/static/media/Marita_Koch.b67369c9.jpg"
+  },
+  {
+    "revision": "57ae9c41989a9c033e580fd8ee08e0fe",
+    "url": "/track-world-records/static/media/Tirunesh_Dibaba.bebdc1f3.jpg"
+  },
+  {
+    "revision": "88c98c6ba5d614203f538899836a9c2b",
+    "url": "/track-world-records/static/media/Usain_Bolt.07a91513.jpg"
+  },
+  {
+    "revision": "ba5461c3720b967ae9241ccd499679e8",
+    "url": "/track-world-records/static/media/Wayde_Van_Niekerk.67178695.jpg"
+  },
+  {
+    "revision": "2ffd54dc84184a7abb12951338a27370",
+    "url": "/track-world-records/static/media/hero.85e8b46e.jpg"
+  },
+  {
+    "revision": "8cf7a18bdcfb37e43eec783ddc6205b8",
+    "url": "/track-world-records/static/media/stopwatch.8cf7a18b.svg"
+  },
+  {
+    "revision": "c15f0170700349a3c777c8019b92c85a",
+    "url": "/track-world-records/static/media/tartan.d56e24b1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/track-world-records/precache-manifest.0474de564cd4313524a568c21d54dbd5.js"
+  "/track-world-records/precache-manifest.b8142b9baf0163d0422f9f11f5c9e1ec.js"
 );
 
 self.addEventListener('message', (event) => {
